4d18dde997c9525a2cd11260fd208746e9e0dadb
Sachin Nagila <sachinnagila2053@gmail.com>
2024-10-16 12:12:20 +1100

    production client root url updated and changed to google smtp

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,13 +2,9 @@ import mongoose from "mongoose";
 
 export const connectToMongoDb = () => {
   try {
-    const connect = mongoose.connect(
-      process.env.DB_CONNECT_URL + "/artistic-nepal"
-    );
+    const connect = mongoose.connect(process.env.DB_CONNECT_URL);
     if (connect) {
-      console.log(
-        `Database conected: ${process.env.DB_CONNECT_URL}/artistic-nepal`
-      );
+      console.log(`Database connected: ${process.env.DB_CONNECT_URL}`);
     }
   } catch (error) {
     console.log("Error:", error);
